9b9a4d6e59435c888859c9bffe08a48f8b8d5b32
odinsam <123679@qq.com>
2022-10-26 05:54:16 +0800

    create template

---
+++ vue3_pinia_less_template/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+</script>
